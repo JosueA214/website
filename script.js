efa2e024b6dc40f1de1b696311f5d83854a6127e
JosueA214 <2423011014@untels.edu.pe>
2025-05-10 18:04:07 -0500

    sd

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function hello () {
     title.innerText = "Hello";
 }
 btn.addEventListener("click", hello);
+
+const input = document.getElementById("input");
+
+input.addEventListener("input", (e) => {
+    console.log(e.target.value);
+    title.innerText = e.target.value;
+});
